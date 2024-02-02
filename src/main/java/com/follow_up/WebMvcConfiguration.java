71dff7c4137caffcf075410e7abf3b8de766b1a2
chnbroa <chnbroa011@naver.com>
2024-02-02 16:19:44 +0900

    오류테스트

--- src/main/java/com/follow_up/WebMvcConfiguration.java
+++ src/main/java/com/follow_up/WebMvcConfiguration.java
@@ -1,11 +1,12 @@
 package com.follow_up;
  
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //import com.model.contents.*;
- 
+@RequiredArgsConstructor
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
@@ -22,15 +23,12 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     }
 
 
-
-
-    //cors 설정
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+                .allowedOrigins("*")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
-
-
 }
