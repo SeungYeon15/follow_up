d128fd263f73032f3f52c0e88b308c181b12b67d
chnbroa <chnbroa011@naver.com>
2024-02-02 15:10:33 +0900

    오류 수정

--- src/main/java/com/follow_up/WebMvcConfiguration.java
+++ src/main/java/com/follow_up/WebMvcConfiguration.java
@@ -10,15 +10,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+ 
+        
         registry.addResourceHandler("/contents/storage/**")
                         .addResourceLocations("file:///" + UploadCon.getUploadDir());
 
         registry.addResourceHandler("/images/storage/**")
                 .addResourceLocations("file:///" + UploadImage.getUploadDir());
-
 //        registry.addResourceHandler("/member/storage/**")
 //                 .addResourceLocations("file:///" + UploadMem.getUploadDir());
     }
 
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://18.217.50.87:3000")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+    }
 }
