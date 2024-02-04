50c22a5694b9a2d018117dfd9e866fc56bc07f24
chnbroa <chnbroa011@naver.com>
2024-02-04 23:32:09 +0900

    오류수정

--- src/main/java/com/follow_up/controller/BC/ImageController.java
+++ src/main/java/com/follow_up/controller/BC/ImageController.java
@@ -15,7 +15,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:3000")
+
 @RestController
 public class ImageController {
     @Autowired
